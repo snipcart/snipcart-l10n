c5c031701ef34092a3851050724b3247b7ffe071
Francis Cote <thatfrankdev@gmail.com>
2019-11-04 15:57:31 -0500

    First rough draft for a localized error message structure

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import Polyglot from 'node-polyglot';
 
 import en from './locales/en.json';
+import { SDKError } from 'snipcart-sdk/core/models/SDKError';
 
 export interface LocalizationService {
     language: string;
     localize: (phrase: string, opts?: any) => string;
+    localizeError: (error: SDKError) => ErrorMessage;
     exists(key: string): boolean;
 }
 
@@ -26,7 +28,50 @@ export default class i18n implements LocalizationService {
         return this.polyglot.t(phrase, opts);
     }
 
+    localizeError(error: SDKError): ErrorMessage {
+        return new LocalizedError(error, this);
+    }
+
     exists(key: string): boolean {
         return this.polyglot.has(key);
+    }
+}
+
+export interface ErrorMessage{
+    title:string;
+    description: string;
+}
+
+// DISCUSS : What do we think about this structure?
+class LocalizedError implements ErrorMessage{
+    title: string;
+    description: string;
+
+    constructor(error: SDKError, i18n: LocalizationService) {
+
+        var parts = error.code.split('.');
+
+        this.title = '';
+        this.description = '';
+
+        console.log(parts);
+
+        if(parts.length <= 2
+            || parts[0] != 'snipcart' 
+            || parts[1] != 'errors'){
+
+            this.title = i18n.localize('errors.default');
+            
+        }
+        else if(parts.length > 2){
+            const errorSubject = parts[2]
+            if(parts.length === 3){
+                this.title = i18n.localize(`errors.${errorSubject}`)
+            }
+            else{
+                this.title = i18n.localize(`errors.${errorSubject}.title`)
+                this.description = i18n.localize(`errors.${errorSubject}.${parts[3]}`);
+            }
+        }
     }
 }
